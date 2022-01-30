89360d27fbaf942cebafe8898ace3a1cbc3c0001
Leo <leo.mulyarchik@gmail.com>
2022-01-30 22:07:31 +0300

    third2.0

--- app1/script.js
+++ app1/script.js
@@ -1,37 +1,72 @@
 "use strict";
-let numberOfFilms = +prompt("Сколько фильмов посмотрел?");
+let numberOfFilms,
+    lastViewedFilm,
+    filmMark;
+
+start();
 
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: true,
 };
 
-if (personalMovieDB.count < 10){
-    alert("Посмотри больше фильмов");
-} else if (10 <= personalMovieDB.count && personalMovieDB.count < 30){
-    alert("Вы норм смотрите");
-} else if (personalMovieDB.count >= 30){
-    alert("Много смотришь, вырубай");
+
+// detectPersonalLevel();
+// rememberMyFilms();
+writeYourGenres();
+showMyDB();
+
+
+
+
+
+
+
+
+function writeYourGenres(){
+    for (let i = 0; i < 3; i++){
+        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`);
+    }
 }
 
-let lastViewedFilm,
-    filmMark;
-    
-for (let i = 0; i != personalMovieDB.count; i++){   
-    
-do {
-    lastViewedFilm = prompt("Один из последних фильмов?");
-} while ( (lastViewedFilm == "") || (lastViewedFilm == undefined) || (lastViewedFilm.length > 50) );
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов посмотрел?");
 
-filmMark = +prompt("Оцените");
-
-personalMovieDB.movies[lastViewedFilm] = filmMark;
-
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов посмотрел?");
+    }
 }
 
-alert("2");
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10) {
+        alert("Посмотри больше фильмов");
+    } else if (10 <= personalMovieDB.count && personalMovieDB.count < 30) {
+        alert("Вы норм смотрите");
+    } else if (personalMovieDB.count >= 30) {
+        alert("Много смотришь, вырубай");
+    }
+}
 
-console.log(personalMovieDB);
+function rememberMyFilms() {
+    for (let i = 0; i != personalMovieDB.count; i++) {
+
+        do {
+            lastViewedFilm = prompt("Один из последних фильмов?");
+        } while ((lastViewedFilm == "") || (lastViewedFilm == undefined) || (lastViewedFilm.length > 50));
+
+        filmMark = +prompt("Оцените");
+
+        personalMovieDB.movies[lastViewedFilm] = filmMark;
+
+    }
+}
+
+function showMyDB(){
+    if (personalMovieDB.privat == false){
+        console.log(personalMovieDB);
+    } 
+}
+
